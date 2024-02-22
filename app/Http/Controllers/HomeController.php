fd5e2003e31218c812faf4ceb7686c0fceb52552
DifanLasama <difannlasama@gmail.com>
2024-02-22 11:10:07 +0700

    feat:add routes and view home

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,4 +8,9 @@ use Illuminate\Http\Request;
 class HomeController extends Controller
 {
     //
+    public function home()
+    {
+    $data = array('title' => 'Home Page');
+    return view ('home',$data);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+
+
 
 Route::get('/', function () {
-    echo "Hello, namaku Denzis";
+    return view('welcome');
+});
+Route::get('/home', function () {
+    return view('home');
+});
+
+// Route::get('/home', [HomeController::class, 'index']);
+/// baris atas ini dijadikan comments
+/// baris bawah ini ditambahkan:
+Route::get('/home', function () {
+    return view('home');
 });
 
